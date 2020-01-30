536d55c7a237b326a56262ee95639e427b50f8d4
Visarut Phusua <aof.dev@gmail.com>
2020-01-30 19:38:16 +0700

    try fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,17 @@
 echo $INPUT_TOKEN
 echo $INPUT_HOST
 echo $INPUT_SOURCES
-echo $INPUT_PROJECTKEY
+echo ${PWD##*/}
 echo $INPUT_PROJECTBASEDIRECTORY
 echo $INPUT_PROJECTNAME
 
 sonar-scanner \
 -Dsonar.host.url="$INPUT_HOST" \
 -Dsonar.login="$INPUT_TOKEN" \
--Dsonar.sources="$INPUT_SOURCES" \
--Dsonar.projectKey="$INPUT_PROJECTKEY" \
+-Dsonar.projectKey=${PWD##*/} \
 -Dsonar.projectBaseDir="$INPUT_PROJECTBASEDIRECTORY" \
 -Dsonar.projectName="$INPUT_PROJECTNAME" \
--Dsonar.exclusions="$INPUT_EXCLUSIONS" \
--Dsonar.inclusions="$INPUT_INCLUSIONS" \
+-Dsonar.sources="$INPUT_SOURCES" \
+# -Dsonar.exclusions="$INPUT_EXCLUSIONS" \
+# -Dsonar.inclusions="$INPUT_INCLUSIONS" \
 -Dsonar.scm.provider=git
