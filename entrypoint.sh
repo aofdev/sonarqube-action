b3b92357ff704b9d1ff2ad82abd088a2857c55b7
Visarut Phusua <aof.dev@gmail.com>
2020-01-30 17:50:20 +0700

    new inputs Sonarqube Scanner with git actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 sonar-scanner \
--Dsonar.login="$INPUT_LOGIN" \
+-Dsonar.login="$INPUT_TOKEN" \
 -Dsonar.host.url="$INPUT_URL" \
--Dsonar.projectBaseDir="$INPUT_BASEDIR" \
+-Dsonar.sources="$INPUT_SOURCES" \
 -Dsonar.projectKey="$INPUT_APP" \
--Dsonar.projectName="$INPUT_APP" \
+-Dsonar.projectName="$INPUT_PROJECTNAME" \
+-Dsonar.inclusions="$INPUT_INCLUSIONS" \
+-Dsonar.exclusions="$INPUT_EXCLUSIONS" \
 -Dsonar.scm.provider=git \
 -Dsonar.sourceEncoding=UTF-8 \
