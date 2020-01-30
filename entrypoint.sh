d6b2cdf1ff667c778aa99769bd50145f1d23636a
Visarut Phusua <aof.dev@gmail.com>
2020-01-30 18:03:40 +0700

    try custom sonarqube #2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+echo $INPUT_TOKEN
+echo $INPUT_HOST
+echo $INPUT_SOURCES
 
 sonar-scanner \
+-Dsonar.host.url="$INPUT_HOST" \
 -Dsonar.login="$INPUT_TOKEN" \
--Dsonar.host.url="$INPUT_URL" \
 -Dsonar.sources="$INPUT_SOURCES" \
 -Dsonar.projectKey="$INPUT_APP" \
 -Dsonar.projectName="$INPUT_PROJECTNAME" \
--Dsonar.inclusions="$INPUT_INCLUSIONS" \
 -Dsonar.exclusions="$INPUT_EXCLUSIONS" \
+# -Dsonar.inclusions="$INPUT_INCLUSIONS" \
 -Dsonar.scm.provider=git \
 -Dsonar.sourceEncoding=UTF-8 \
